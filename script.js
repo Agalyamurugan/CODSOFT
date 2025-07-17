b85ac9d2adbf046cfb457170c846fc12544aa9c1
Agalyamurugan <agalyamurugan0280@gmail.com>
2025-07-17 19:06:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function appendValue(value) {
+  document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = display.value.slice(0,-1);
+}
+
+function calculate() {
+  try {
+    const result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+  }
+}
